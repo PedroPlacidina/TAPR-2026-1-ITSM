2a1821984b557367d451167342c0370ca41e3655
PEDRO HENRIQUE PLACIDINA MARIA <pedro.maria@univille.br>
2026-04-10 20:02:01 -0300

    adicionando az function 2

--- src/function_app.py
+++ src/function_app.py
@@ -3,10 +3,19 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def timer_trigger_1(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('Python timer trigger function executed.')
+    logging.info('Executando azure function 1')
+
+@app.timer_trigger(schedule="0 */3 * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def timer_trigger_2(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Executando azure function 2')
