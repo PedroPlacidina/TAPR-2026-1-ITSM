7a306e5e2b2ec3952171abf6962f5ac136dfed9a
Pedro Henrique Placidina <pedroplacidina14@gmail.com>
2026-06-14 15:19:19 -0300

    corrigindo erros

--- src/function_app.py
+++ src/function_app.py
@@ -13,6 +13,7 @@ from triggers.extract_fila import bp as extract_fila
 from triggers.extract_sla import bp as extract_sla
 from triggers.extract_solicitante import bp as extract_solicitante
 from triggers.extract_statushistorico import bp as extract_statushistorico
+from triggers.extract_csat_avaliacao import bp as extract_csat_avaliacao
 
 app.register_functions(extract_analista)
 app.register_functions(extract_categoria)
@@ -24,3 +25,4 @@ app.register_functions(extract_fila)
 app.register_functions(extract_sla)
 app.register_functions(extract_solicitante)
 app.register_functions(extract_statushistorico)
+app.register_functions(extract_csat_avaliacao)
